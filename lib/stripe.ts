5f879c40b55e62dae41012a69d56d032ec29a111
Ahmed Adoha <ahmed.adoha@gmail.com>
2026-04-26 09:15:46 +0200

    fix: update stripe apiVersion

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -6,6 +6,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2024-12-18.acacia', // Updated to match likely installed version or standard
+    apiVersion: '2025-12-15.clover', // Updated to match installed version
     typescript: true,
 });
